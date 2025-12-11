116a47fcb86f71b047ede4ede292d07a0f41aa37
MRezaKarimi <mr.karimi77@yahoo.com>
2025-12-11 17:43:57 +0330

    feat: reduce padding

--- frontend/src/components/control-bar/AdditionalControls.vue
+++ frontend/src/components/control-bar/AdditionalControls.vue
@@ -8,7 +8,7 @@ import {
 </script>
 
 <template>
-  <div class="flex items-center gap-4 pe-8">
+  <div class="flex items-center gap-4 pe-4">
     <IconPlaylistAdd stroke="1.75" size="24" class="cursor-pointer" />
     <IconVolume stroke="1.75" size="20" class="cursor-pointer" />
     <IconAdjustments stroke="1.75" size="20" class="cursor-pointer" />
